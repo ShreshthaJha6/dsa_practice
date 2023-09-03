34fee207216370d9a1b0a9e5104757a938baa4a1
ShreshthaJha6 <122299502+ShreshthaJha6@users.noreply.github.com>
2023-09-04 01:24:10 +0530

    Create contains_duplicate.cpp

---
+++ contains_duplicate.cpp
@@ -0,0 +1,7 @@
+//given any integer array- nums. 
+//returns TRUE if any value appears at least twice and FALSE if every statement is distinct
+//MY IDEA: can use linear search. make int count=0 and in case of duplicate- count++. if count>0 return true; else return false;
+// time: O(n^2) memory: O(1)
+//SORTING: all the duplicates will come adjacent to each other. so we will have to move through the array once.
+//time: O(nlong) memory: O(1)
+// USE HASHSET
