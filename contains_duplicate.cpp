3c4d1e2f1213473d3063ccd3913e136a30e99aad
ShreshthaJha6 <122299502+ShreshthaJha6@users.noreply.github.com>
2023-09-04 21:31:27 +0530

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -5,3 +5,20 @@
 //SORTING: all the duplicates will come adjacent to each other. so we will have to move through the array once.
 //time: O(nlong) memory: O(1)
 // USE HASHSET
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i])!=s.end()){
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
